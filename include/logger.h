eb418fb3a5ecae12b680a61fb0f2b893d364a65a
BuildTools <unconfigured@null.spigotmc.org>
2025-09-23 22:05:29 +0200

    Multi setup and 1st window

---
+++ include/logger.h
@@ -0,0 +1,11 @@
+#include <string>
+
+class Logger {
+public:
+    static void log(const std::string& message);
+    static void warn(const std::string& message); 
+    static void error(const std::string& message);
+    static void info(const std::string& message);
+private:
+    static std::string getTime();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "logger.h"
+#include "raylib.h"
 
 int main(int argc, char *argv[]){
-    std::cout << "Hello world?";
+    Logger::info("Starting application");
+    
+    InitWindow(800, 600, "Adventure Test");
+    
+    Logger::info("Window initialized");
+    
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(DARKBLUE);
+        DrawText("Hello Raylib!", 10, 10, 20, WHITE);
+        EndDrawing();
+    }
+    
+    CloseWindow();
+    Logger::info("Application closed");
+    
     return 0;
 }
